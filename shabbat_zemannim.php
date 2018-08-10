10b76e3e1e57e91b5524ddefb55b66a69f0f4314
Luna Lunapiena <lunacodes@gmail.com>
2018-08-09 21:38:42 -0400

    prefix all functions with shabbat to avoid conflicts with the luna_zemanim_widget

--- shabbat_zemannim.php
+++ shabbat_zemannim.php
@@ -2,20 +2,20 @@
 /**
 * Plugin Name: Shabbat Zemmanim
  * Plugin URI: https://lunacodesdesign.com/
- * Description: Displays Zemannim (times) according to Sepharadic tradition. 
- *   Uses the DB-IP API and the Google Maps API for geographic information. 
+ * Description: Displays Zemannim (times) according to Sepharadic tradition.
+ *   Uses the DB-IP API and the Google Maps API for geographic information.
  *   Uses the Sun-Calc Library (https://github.com/mourner/suncalc) for sunrise/sunset information.
  * Version: 1.1
  * Author: Luna Lunapiena
  * Author URI: https://lunacodesdesign.com/
  * License: GPL3+
- * License URI: https://www.gnu.org/licenses/gpl-3.0.html 
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
  * Text Domain: shabbat_zemanim_widget_domain
  * Change Record:
  * ***********************************
  * 2018- - initial creation
  *
- * 
+ *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
  *   the Free Software Foundation,version 3
@@ -32,10 +32,14 @@
 /**
  * Issues:
  * ***********************************
- * getGeoDetails: var state needs For Loop, instead of just being set to null
+ * shabbatGetGeoDetails: var state needs For Loop, instead of just being set to null
  * improve code logic with promises?
  * Why Did I need to change outputZemanim to outputShabbatZemannim
  * shabbatDateStr isn't outputting
+ * Needs to be refactored using Classes to avoid conflicts w/ other Zemanim plugin
+   * Is there a way to check if we already have some of the info from the other plugin???
+ * shabbatCheckForDST() isn't being used - delete?
+ * Replace unxiTimestampToDate w/ PHP
 */
 
 class Shabbat_Zemannim_Widget extends WP_Widget {
@@ -48,16 +52,16 @@ class Shabbat_Zemannim_Widget extends WP_Widget {
       'shabbat_zemanim_widget', // Base ID
       __('Shabbat Zemannim', 'shabbat_zemanim_widget_domain'), // Name
       array( 'description' => __( "Displays Zemannim (times) according to Sepharadic tradition", 'shabbat_zemanim_widget_domain' ),  ) //Args
-    ); 
+    );
 
   add_action( 'widgets_init', function() {register_widget( 'Shabbat_Zemannim_Widget' ); } );
   }
 
   /**
    * Front-end display of widget.
-   * 
+   *
    * @see WP_Widget::widget()
-   * 
+   *
    * @param array $args     Widget Arguments.
    * @param array $instance Saved values from database   */
   public function widget( $args, $instance ) {
@@ -72,8 +76,8 @@ class Shabbat_Zemannim_Widget extends WP_Widget {
 
   function getShabbatDate() {
     /* Get Shabbat Date */
-    
-    // If Friday or Sat, don't get next week's date 
+
+    // If Friday or Sat, don't get next week's date
     if (date('N') == 5) {
       $friday = strtotime("now");
     } elseif (date('N') == 6) {
@@ -86,7 +90,7 @@ class Shabbat_Zemannim_Widget extends WP_Widget {
     $friday_m = date("m", $friday);
     $friday_d = date("j", $friday);
     $friday_y = date("Y", $friday);
-    
+
     // Get Friday's Hebrew Date via hebcal
     $hebDateUrl = "https://www.hebcal.com/converter/?cfg=json&gy=$friday_y&gm=$friday_m&gd=$friday_d&g2h=1";
 
@@ -99,11 +103,11 @@ class Shabbat_Zemannim_Widget extends WP_Widget {
     $data = json_decode($results, true);
     $hebDateStr = $data['hebrew'];
 
-    // Get the name of the month 
+    // Get the name of the month
     $dateObj   = DateTime::createFromFormat('!m', $friday_m);
     $monthName = $dateObj->format('F'); // March
     $fridayDateStr = "$monthName $friday_d, $friday_y";
-    
+
     $fridayDateInfo = [$fridayDateStr, $hebDateStr];
     // Debugging
     // echo("Date Info: ");
@@ -117,7 +121,7 @@ class Shabbat_Zemannim_Widget extends WP_Widget {
   $ShabbatDateInfo = getShabbatDate();
   // echo($ShabbatDateInfo);
 
-  function outputShabbatZemannim($dateInfo) { 
+  function outputShabbatZemannim($dateInfo) {
     // echo("Date Info: $dateInfo");
     // Get Friday's Hebrew Date via hebcal
     $shabbatDateStr = $dateInfo[0];
@@ -149,14 +153,14 @@ outputShabbatZemannim($ShabbatDateInfo);
   var z_shema = document.getElementById("zemanim_shema");
   var z_minha = document.getElementById("zemanim_minha");
   var z_peleg = document.getElementById("zemanim_peleg");
-  var z_sunset = document.getElementById("zemanim_sunset");    
+  var z_sunset = document.getElementById("zemanim_sunset");
   var zemanim = document.getElementById("zemanim_container");
 
   /**
-   * getLocation - gets user's lat & long via HTML5 Geolocation API sends to getGeoDetails
+   * shabbatGetLocation - gets user's lat & long via HTML5 Geolocation API sends to shabbatGetGeoDetails
    * @return {(number|Array)} [lat, long] coordinates
    */
-  function getLocation() {
+  function shabbatGetLocation() {
     var options = {
       enableHighAccuracy: true,
       maximumAge: 0
@@ -165,28 +169,28 @@ outputShabbatZemannim($ShabbatDateInfo);
     function error(err) {
       console.warn(`ERROR(${err.code}): ${err.message}`);
     zemanim.innerHtml = "Please enable location services to display the most up-to-date Zemanim";
-          getAddrDetailsByIp();
+          shabbatGetAddrDetailsByIp();
     }
 
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(getLatLngByGeo, error, options);
+        navigator.geolocation.getCurrentPosition(shabbatGetLatLngByGeo, error, options);
       }
   }
 
   /**
-   * getLatLngByGeo - separate [lat, long] int lat & long vars, pass to Google Maps API via getGeoDetails
+   * shabbatGetLatLngByGeo - separate [lat, long] int lat & long vars, pass to Google Maps API via shabbatGetGeoDetails
    * @param  {number|Array} position [lat, long]
    * @return {[type]}          [description]
    */
-  function getLatLngByGeo(position) {
+  function shabbatGetLatLngByGeo(position) {
     var pos = position;
     var lat = pos.coords.latitude;
     var long = pos.coords.longitude;
 
-    getGeoDetails(lat, long);
+    shabbatGetGeoDetails(lat, long);
   }
 
-  function getAddrDetailsByIp() {
+  function shabbatGetAddrDetailsByIp() {
     let urlStr = 'https://api.db-ip.com/v2/free/self';
     fetch(urlStr)
       .then(function(response) {
@@ -203,11 +207,11 @@ outputShabbatZemannim($ShabbatDateInfo);
         let url = urlBase + "&address=" + address + "&components=" + country + "&key=" + apiKey;
         // use regEx to replace all spaces with plus signs
         let urlStr = url.replace(/\s+/g, "+");
-        getLatLongByAddr(urlStr);
+        shabbetGetLatLongByAddr(urlStr);
       });
   }
 
-  function getLatLongByAddr(urlStr) {
+  function shabbetGetLatLongByAddr(urlStr) {
     let url = urlStr;
     fetch(url)
       .then((response) => {
@@ -217,17 +221,17 @@ outputShabbatZemannim($ShabbatDateInfo);
         let data = new Array(res.results[0]);
         let lat = data[0].geometry.location.lat;
         let long = data[0].geometry.location.lng;
-        getGeoDetails(lat, long);
+        shabbatGetGeoDetails(lat, long);
       });
   }
 
   /**
-   * [getGeoDetails feed lat & long coords into Google Maps API to obtain City, State info and pass to generateTimes ]
+   * [shabbatGetGeoDetails feed lat & long coords into Google Maps API to obtain City, State info and pass to generateTimes ]
    * @param  {[float]} lat_crd  [user's lattitude]
    * @param  {[float]} long_crd [user's longitude]
    * @return {[string]} cityStr [user's City, State]
    */
-  function getGeoDetails(lat_crd, long_crd) {
+  function shabbatGetGeoDetails(lat_crd, long_crd) {
     let lat = lat_crd;
     let long = long_crd;
     var point = new google.maps.LatLng(lat, long);        new google.maps.Geocoder().geocode({'latLng': point}, function (res, status) {
@@ -250,11 +254,11 @@ outputShabbatZemannim($ShabbatDateInfo);
         var cityStr =  city + ", " + state;
       }
 
-      generateTimes(lat, long, cityStr);
+      shabbatGenerateTimes(lat, long, cityStr);
     });
   }
 
-  function checkForDST() {
+  function shabbatCheckForDST() {
     Date.prototype.stdTimezoneOffset = function () {
       var jan = new Date(this.getFullYear(), 0, 1);
       var jul = new Date(this.getFullYear(), 6, 1);
@@ -266,64 +270,76 @@ outputShabbatZemannim($ShabbatDateInfo);
     }
 
     var today = new Date();
-    if (today.isDstObserved()) { 
+    if (today.isDstObserved()) {
       return true;
     }
   }
 
-  function formatTime(x) {
+  function shabbatFormatTime(x) {
     var reformattedTime = x.toString();
     reformattedTime = ("0" + x).slice(-2);
     return reformattedTime;
   }
 
-  function generateTimeStrings(timeObj) {
+  function shabbatGenerateTimeStrings(timeObj) {
     var year = timeObj.getFullYear();
-    var month = formatTime(timeObj.getMonth() + 1);
-    var day = formatTime(timeObj.getDate());
-    var hour = formatTime(timeObj.getHours());
-    var min = formatTime(timeObj.getMinutes());
-    var sec = formatTime(timeObj.getSeconds());
+    var month = shabbatFormatTime(timeObj.getMonth() + 1);
+    var day = shabbatFormatTime(timeObj.getDate());
+    var hour = shabbatFormatTime(timeObj.getHours());
+    var min = shabbatFormatTime(timeObj.getMinutes());
+    var sec = shabbatFormatTime(timeObj.getSeconds());
     var buildTimeStr = year + "-" + month + "-" + day + " " + hour + ":" + min;
     return buildTimeStr;
   }
 
-  function generateDateString(timeObj) {
-    var monthInt = timeObj.getMonth();
-    var monthList = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    var month = monthList[monthInt];
-    var day = formatTime(timeObj.getDate());
-    var year = timeObj.getFullYear();
-    var buildDateStr = '<span id="zemanin_date">' + "Times for " + month + " " + day + ", " + year + '</span>';
-    return buildDateStr;
-  }
+  // function generateDateString(timeObj) {
+  //   var monthInt = timeObj.getMonth();
+  //   var monthList = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  //   var month = monthList[monthInt];
+  //   var day = shabbatFormatTime(timeObj.getDate());
+  //   var year = timeObj.getFullYear();
+  //   var buildDateStr = '<span id="zemanin_date">' + "Times for " + month + " " + day + ", " + year + '</span>';
+  //   return buildDateStr;
+  // }
 
-  function generateTimes(lat, long, city) {
+  function shabbatGenerateTimes(lat, long, city) {
     var cityStr = city;
-    var times = SunCalc.getTimes(new Date(), lat, long);
+    var times = SunCalc.getTimes(new Date(<?php echo("'$ShabbatDateInfo[0]'"); ?>), lat, long);
     var sunriseObj = times.sunrise;
     var offSet = sunriseObj.getTimezoneOffset() / 60;
     var offSetSec = offSet * 3600;
     var dateObj = new Date();
-    var dateStr = generateDateString(dateObj);
-    var sunriseStr = generateTimeStrings(sunriseObj);
+    // var dateStr = generateDateString(dateObj);
+    var sunriseStr = shabbatGenerateTimeStrings(sunriseObj);
     var sunsetObj = times.sunset;
-    var sunsetStr = generateTimeStrings(sunsetObj);
+    var sunsetStr = shabbatGenerateTimeStrings(sunsetObj);
+
+    console.log("Generated Times");
+    console.log(cityStr);
+    console.log(times);
+    console.log(sunriseObj);
+    console.log(offSet);
+    console.log(offSetSec);
+    console.log(dateObj);
+    // console.log(dateStr);
+    console.log(sunriseStr);
+    console.log(sunsetObj);
+    console.log(sunsetStr);
 
     var SunriseDateTimeInt = parseFloat((new Date(sunriseStr).getTime() / 1000) - offSetSec);
     var SunsetDateTimeInt = parseFloat((new Date(sunsetStr).getTime() / 1000) - offSetSec);
     var sunriseSec = SunriseDateTimeInt - offSet;
     var sunsetSec = SunsetDateTimeInt - offSet;
 
-    var latestShemaStr = '<span id="zmantitle">Latest Shema: </span>' + calculateLatestShema(sunriseSec, sunsetSec, offSetSec);
-    var earliestMinhaStr = '<span id="zmantitle">Earliest Minḥa: </span>' + calculateEarliestMinha(sunriseSec, sunsetSec, offSetSec);
-    var pelegHaMinhaStr = '<span id="zmantitle">Peleḡ HaMinḥa: </span>' + calculatePelegHaMinha(sunriseSec, sunsetSec, offSetSec);
-    var displaySunsetStr = '<span id="zmantitle">Sunset: </span>' + unixTimestampToDate(SunsetDateTimeInt+offSetSec);
+    var latestShemaStr = '<span id="zmantitle">Latest Shema: </span>' + shabbatCalculateLatestShema(sunriseSec, sunsetSec, offSetSec);
+    var earliestMinhaStr = '<span id="zmantitle">Earliest Minḥa: </span>' + shabbatCalculateEarliestMinha(sunriseSec, sunsetSec, offSetSec);
+    var pelegHaMinhaStr = '<span id="zmantitle">Peleḡ HaMinḥa: </span>' + shabbatCalculatePelegHaMinha(sunriseSec, sunsetSec, offSetSec);
+    var displaySunsetStr = '<span id="zmantitle">Sunset: </span>' + shabbatUnixTimestampToDate(SunsetDateTimeInt+offSetSec);
 
-    displayTimes(dateStr, cityStr, latestShemaStr, earliestMinhaStr, pelegHaMinhaStr, displaySunsetStr);
+    shabbatDisplayTimes(cityStr, latestShemaStr, earliestMinhaStr, pelegHaMinhaStr, displaySunsetStr);
   }
 
-  function unixTimestampToDate(timestamp) {
+  function shabbatUnixTimestampToDate(timestamp) {
     var date = new Date(timestamp * 1000);
     var hours = date.getHours();
     var ampm = "AM";
@@ -340,31 +356,31 @@ outputShabbatZemannim($ShabbatDateInfo);
     return formattedTime + " " + ampm;
   }
 
-  function calculateLatestShema(sunriseSec, sunsetSec, offSetSec) {
+  function shabbatCalculateLatestShema(sunriseSec, sunsetSec, offSetSec) {
     var halakhicHour = Math.abs((sunsetSec - sunriseSec) / 12);
     var shemaInSeconds = sunriseSec + (halakhicHour * 3) + offSetSec;
-    var latestShema = unixTimestampToDate(shemaInSeconds);
+    var latestShema = shabbatUnixTimestampToDate(shemaInSeconds);
 
     return latestShema;
   }
 
-  function calculateEarliestMinha(sunriseSec, sunsetSec, offSetSec) {
+  function shabbatCalculateEarliestMinha(sunriseSec, sunsetSec, offSetSec) {
     var halakhicHour = (sunsetSec - sunriseSec) / 12;
     var minhaInSeconds = sunriseSec + (halakhicHour * 6.5) + offSetSec;
-    var earliestMinha = unixTimestampToDate(minhaInSeconds);
+    var earliestMinha = shabbatUnixTimestampToDate(minhaInSeconds);
 
     return earliestMinha;
   }
 
-  function calculatePelegHaMinha(sunriseSec, sunsetSec, offSetSec) {
+  function shabbatCalculatePelegHaMinha(sunriseSec, sunsetSec, offSetSec) {
     var halakhicHour = (sunsetSec - sunriseSec) / 12;
     var minhaInSeconds = sunsetSec - (halakhicHour * 1.25) + offSetSec;
-    var pelegHaMinha = unixTimestampToDate(minhaInSeconds);
+    var pelegHaMinha = shabbatUnixTimestampToDate(minhaInSeconds);
 
     return pelegHaMinha;
   }
 
-  function displayTimes(date, city, shema, minha, peleg, sunset) {
+  function shabbatDisplayTimes(city, shema, minha, peleg, sunset) {
 
     // z_date.innerHTML = date + "<br>";
     z_city.innerHTML = city + "<br>";
@@ -376,7 +392,7 @@ outputShabbatZemannim($ShabbatDateInfo);
 
   // Make sure we're ready to run our script!
   jQuery(document).ready(function($) {
-    getLocation();
+    shabbatGetLocation();
   });
 
 </script>
@@ -389,9 +405,9 @@ outputShabbatZemannim($ShabbatDateInfo);
 
 /**
  * Back-end widget form.
- * 
+ *
  * @see WP_Widget::form()
- * 
+ *
  * @param array $instance Previously saved values from database.
  */
   public function form( $instance ) {
@@ -405,19 +421,19 @@ outputShabbatZemannim($ShabbatDateInfo);
   // Widget admin form
 ?>
   <p>
-    <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
+    <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
     <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
   </p>
 <?php
-} 
+}
 
 /**
  * Sanitize widget form values as they are saved.
- * 
+ *
  * @see WP_Widget::update()
- * 
+ *
  * @param array $new instance Values just sent to be saved from database.
- * 
+ *
  * @return array Updated safe values to be saved.
  */
 public function update( $new_instance, $old_instance ) {
@@ -425,7 +441,7 @@ $instance = array();
 $instance['title'] = ( !empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
 
 return $instance;
-}  
+}
 
 } // class Shabbat_Zemannim_Widget
 
