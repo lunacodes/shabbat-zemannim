6b46c5fff70348647a215d68699e83efc4b6ece1
Luna Lunapiena <lunacodes@gmail.com>
2018-08-09 21:41:11 -0400

    remove shabbatCheckForDST() - unused function

--- shabbat_zemannim.php
+++ shabbat_zemannim.php
@@ -258,23 +258,6 @@ outputShabbatZemannim($ShabbatDateInfo);
     });
   }
 
-  function shabbatCheckForDST() {
-    Date.prototype.stdTimezoneOffset = function () {
-      var jan = new Date(this.getFullYear(), 0, 1);
-      var jul = new Date(this.getFullYear(), 6, 1);
-      return Math.max(jan.getTimezoneOffset(), jul.getTimezoneOffset());
-  }
-
-    Date.prototype.isDstObserved = function () {
-      return this.getTimezoneOffset() < this.stdTimezoneOffset();
-    }
-
-    var today = new Date();
-    if (today.isDstObserved()) {
-      return true;
-    }
-  }
-
   function shabbatFormatTime(x) {
     var reformattedTime = x.toString();
     reformattedTime = ("0" + x).slice(-2);
